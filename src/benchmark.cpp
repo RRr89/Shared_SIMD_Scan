21a1742c8669f8658560422cd489f2fdc4b7a938
Andreas Bauer <andreas.bauer1@stud.tu-darmstadt.de>
2018-08-15 23:16:49 +0200

    Got rid of repetition in benchmark code.

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <sstream>
 #include <memory>
+#include <functional>
 
 static const size_t data_size = 500 * 1 << 20;
 static const char* const data_size_str = "500 MB";
@@ -19,27 +20,48 @@ std::chrono::nanoseconds _clock()
     return elapsed;
 }
 
-void print_numbers(const char* benchmark, const size_t elapsed_time_us[5])
+void print_numbers(std::string benchmark_name, const size_t elapsed_time_us[5])
 {
-    std::cout << benchmark << ": [" << (elapsed_time_us[0]/1000000)
-                           << ", "  << (elapsed_time_us[1]/1000000)
-                           << ", "  << (elapsed_time_us[2]/1000000)
-                           << ", "  << (elapsed_time_us[3]/1000000)
-                           << ", "  << (elapsed_time_us[4]/1000000)
-                           << "] ms" << std::endl;
+    std::cout << benchmark_name
+        << ": [" << (elapsed_time_us[0]/1000000)
+        << ", "  << (elapsed_time_us[1]/1000000)
+        << ", "  << (elapsed_time_us[2]/1000000)
+        << ", "  << (elapsed_time_us[3]/1000000)
+        << ", "  << (elapsed_time_us[4]/1000000)
+        << "] ms" << std::endl;
 }
 
 bool check_result(std::vector<uint16_t> input, int* output, size_t size)
 {
-    for (size_t i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
+	{
+		if (input[i] != output[i])
+		{
+			std::cout << "first mismatch at index " << i << std::endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+void do_decompression_benchmark(
+	std::string name, 
+	std::vector<uint16_t> input,
+	size_t input_size, 
+	__m128i* compressed_data,
+	std::function<void(__m128i*, size_t, int*)> decompression_function) 
+{
+    size_t elapsed_time_us[5];
+    std::unique_ptr<int[]> output_buffer = std::make_unique<int[]>(input_size);
+
+    for (int i = 0; i < 5; ++i)
     {
-        if (input[i] != output[i])
-        {
-            std::cout << "first mismatch at index " << i << std::endl;
-            return false;
-        }
+        _clock();
+        decompression_function(compressed_data, input_size, output_buffer.get());
+        elapsed_time_us[i] = _clock().count();
     }
-    return true;
+    check_result(input, output_buffer.get(), input_size);
+    print_numbers(name, elapsed_time_us);
 }
 
 void bench_decompression()
@@ -47,7 +69,6 @@ void bench_decompression()
     size_t compression = 9;
     size_t buffer_target_size = data_size;
     size_t input_size = buffer_target_size * 8 / compression;
-    size_t elapsed_time_us[5];
 
     std::vector<uint16_t> input(input_size);
     for (size_t i = 0; i < input_size; i++)
@@ -58,107 +79,28 @@ void bench_decompression()
     __m128i* compressed = (__m128i*) compress_9bit_input(input);
 
     std::cout.imbue(std::locale(""));
-
     std::cout << "compressed input: " << input_size << " (" << buffer_target_size << " bytes)" << std::endl;
 
-    std::unique_ptr<int[]> output_buffer = std::make_unique<int[]>(input_size);
-
     // ------------
 
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_standard(compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    print_numbers("unvectorized", elapsed_time_us);
-
-    // ------------
-
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_128_sweep((__m128i*)compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    check_result(input, output_buffer.get(), input_size);
-    print_numbers("sse 128 (sweep)", elapsed_time_us);
-
-    // ------------
-
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_128_nosweep((__m128i*)compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    check_result(input, output_buffer.get(), input_size);
-    print_numbers("sse 128 (load after 4)", elapsed_time_us);
-
-    // ------------
-
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_128_9bit((__m128i*)compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    check_result(input, output_buffer.get(), input_size);
-    print_numbers("sse 128 (9bit optimized masks)", elapsed_time_us);
-
-    // ------------
-
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_128((__m128i*)compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    check_result(input, output_buffer.get(), input_size);
-    print_numbers("sse 128 (optimized masks)", elapsed_time_us);
-
-    // ------------
-
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_128_aligned((__m128i*)compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    check_result(input, output_buffer.get(), input_size);
-    print_numbers("sse 128 (optimized masks + aligned loads)", elapsed_time_us);
-
-    // ------------
+	do_decompression_benchmark("unvectorized", input, input_size, compressed, decompress_standard);
+	do_decompression_benchmark("sse 128 (sweep)", input, input_size, compressed, decompress_128_sweep);
+	do_decompression_benchmark("sse 128 (load after 4)", input, input_size, compressed, decompress_128_nosweep);
+	do_decompression_benchmark("sse 128 (9 bit optimized masks)", input, input_size, compressed, decompress_128_9bit);
+	do_decompression_benchmark("sse 128 (optimized masks)", input, input_size, compressed, decompress_128);
+	do_decompression_benchmark("sse 128 (optimized masks + aligned loads)", input, input_size, compressed, decompress_128_aligned);
 
 #ifdef __AVX__
+	do_decompression_benchmark("avx 256", input, input_size, compressed, decompress_256);
 
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_256((__m128i*)compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    check_result(input, output_buffer.get(), input_size);
-    print_numbers("avx 256", elapsed_time_us);
-
-    // ------------
 #ifdef __AVX2__
-
-    for (int i=0; i<5; ++i)
-    {
-        _clock();
-        decompress_256_avx2((__m128i*)compressed, input_size, output_buffer.get());
-        elapsed_time_us[i] = _clock().count();
-    }
-    check_result(input, output_buffer.get(), input_size);
-    print_numbers("avx 256 (avx2 shift)", elapsed_time_us);
-
+	do_decompression_benchmark("avx 256 (avx2 shift)", input, input_size, compressed, decompress_256_avx2);
 #endif
 #else
-    std::cout << "avx 256 is not supported" << std::endl;
+	std::cout << "avx 256 is not supported" << std::endl;
 #endif
 
-    std::cout << "finished benchmark" << std::endl;
+	std::cout << "finished benchmark" << std::endl;
 }
 
 void bench_memory()
