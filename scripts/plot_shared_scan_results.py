80679102614d9a3c669f5ae36b25c464f5a9b038
Andreas Bauer <andreas.bauer1@stud.tu-darmstadt.de>
2018-09-30 22:49:37 +0200

    Updates to plotting code.

--- scripts/plot_shared_scan_results.py
+++ scripts/plot_shared_scan_results.py
@@ -10,46 +10,85 @@ if len(sys.argv) < 2 or not os.path.isfile(sys.argv[1]):
 
 df = pd.read_csv(sys.argv[1], comment='#')
 
+# set default data size for older measurements that don't have them
+if not 'data_size' in df.columns:
+    df['data_size'] = 30
+
+# sort the columns used for x axis so plotting works properly...
+df = df.sort_values(by=['predicate_count', 'data_size'])
+
+# add this column
 df['runtime_per_predicate'] = df['avg_runtime_ms'] / df['predicate_count']
 
-# figure 1: absolute
-plt.figure()
-variants = [v for v in df['variant'].unique() if v.startswith('sse')]
-for v in variants:
-    df1 = df[df['variant'] == v]
-    plt.plot(df1['predicate_count'], df1['avg_runtime_ms'], label=v)
+# x axis is predicate count; given data size
+def plot_predicates(df, data_size, types=[1,2,3]):
+    df = df[df['data_size'] == data_size]
+    if df.empty:
+        print('No data.')
+        return
 
-plt.title('Shared Scan performance (absolute)')
-plt.xlabel('# predicates')
-plt.ylabel('ms')
-plt.legend()
+    if 1 in types:
+        # figure 1: absolute
+        plt.figure()
+        variants = [v for v in df['variant'].unique()]
+        for v in variants:
+            df1 = df[df['variant'] == v]
+            plt.plot(df1['predicate_count'], df1['avg_runtime_ms'], label=v)
 
-# figure 2: relative to predicate count
-plt.figure()
-variants = [v for v in df['variant'].unique() if v.startswith('sse')]
-for v in variants:
-    df1 = df[df['variant'] == v]
-    plt.plot(df1['predicate_count'], df1['runtime_per_predicate'], label=v)
+        plt.title('Shared Scan performance (absolute); data: {} MiB'.format(data_size))
+        plt.xlabel('# predicates')
+        plt.ylabel('ms')
+        plt.legend()
 
-plt.title('Shared Scan performance (relative to predicate count)')
-plt.xlabel('# predicates')
-plt.ylabel('ms / predicate')
-plt.legend()
+    if 2 in types:
+        # figure 2: relative to predicate count
+        plt.figure()
+        variants = [v for v in df['variant'].unique()]
+        for v in variants:
+            df1 = df[df['variant'] == v]
+            plt.plot(df1['predicate_count'], df1['runtime_per_predicate'], label=v)
 
-# figure 3: improvement to sequential
-plt.figure()
+        plt.title('Shared Scan performance (relative to predicate count); data: {} MiB'.format(data_size))
+        plt.xlabel('# predicates')
+        plt.ylabel('ms / predicate')
+        #plt.gca().set_ylim(0, 20)
+        plt.legend()
 
-g = df.groupby(df['predicate_count'])
-rows = []
-for name, group in g:
-    r1 = group[group['variant'] == 'sse 128, sequential (unrolled)']['avg_runtime_ms'].values[0]
-    r2 = group[group['variant'] == 'sse 128, standard']['avg_runtime_ms'].values[0]
-    rows.append({'predicate_count': name, 'improvement': 1 - r2 / r1})
-df3 = pd.DataFrame(rows)
+    if 3 in types:
+        # figure 3: improvement to sequential
+        plt.figure()
 
-plt.bar(df3['predicate_count'], df3['improvement'], width=1)
-plt.title('Improvement standard vs sequential (unrolled)')
-plt.xlabel('# predicates')
-plt.ylabel('improvement')
-plt.gca().set_ylim(-0.5, 0.5)
-plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1.0))
+        g = df.groupby(df['predicate_count'])
+        rows = []
+        for name, group in g:
+            r1 = group[group['variant'] == 'sse 128, sequential (unrolled)']['avg_runtime_ms'].values[0]
+            r2 = group[group['variant'] == 'sse 128, standard']['avg_runtime_ms'].values[0]
+            rows.append({'predicate_count': name, 'improvement': 1 - r2 / r1})
+        df3 = pd.DataFrame(rows)
+
+        plt.bar(df3['predicate_count'], df3['improvement'], width=1)
+        plt.title('Improvement standard vs sequential (unrolled); data: {} MiB'.format(data_size))
+        plt.xlabel('# predicates')
+        plt.ylabel('improvement')
+        plt.gca().set_ylim(-0.5, 0.5)
+        plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1.0))
+
+
+# x axis is data size; given predicate count
+def plot_data_size(df, predicate_count):
+    df = df[df['predicate_count'] == predicate_count]
+    if df.empty:
+        print('No data.')
+        return
+
+    # figure 1: absolute
+    plt.figure()
+    variants = [v for v in df['variant'].unique()]
+    for v in variants:
+        df1 = df[df['variant'] == v]
+        plt.plot(df1['data_size'], df1['avg_runtime_ms'], label=v)
+
+    plt.title('Shared Scan performance (absolute); {} predicates'.format(predicate_count))
+    plt.xlabel('data size (MiB)')
+    plt.ylabel('ms')
+    plt.legend()
--- scripts/prepare_shared_scan_results.py
+++ scripts/prepare_shared_scan_results.py
@@ -9,19 +9,23 @@ if len(sys.argv) < 2:
 
 simdscan = sys.argv[1]
 csv_writer = csv.writer(sys.stdout)
-csv_writer.writerow(['predicate_count', 'variant', 'avg_runtime_ms'])
+csv_writer.writerow(['data_size', 'predicate_count', 'variant', 'avg_runtime_ms'])
 
-def parse_output(predicate_count, output):
+def parse_output(data_size, predicate_count, output):
     for line in output.splitlines():
         if not line.startswith('*'):
             continue
         variant = line[2:].split(': ')[0]
         avg_runtime_ms = line.split(': ')[1].split('; ')[0][:-2]
-        csv_writer.writerow([predicate_count, variant, avg_runtime_ms])
+        csv_writer.writerow([data_size, predicate_count, variant, avg_runtime_ms])
 
-for i in range(1, 513):
-    predicate_count = i
-    print('# predicate count: {}'.format(predicate_count))
-    result = subprocess.run([simdscan, '30', '3', 'sharedscan', str(predicate_count)], stdout=subprocess.PIPE)
+def run(data_size, predicate_count, repetitions):
+    print('# data size: {}, predicate count: {}'.format(data_size, predicate_count))
+    result = subprocess.run([simdscan, str(data_size), str(repetitions), 'sharedscan', str(predicate_count)], stdout=subprocess.PIPE)
     print('# {}'.format(result.args))
-    parse_output(predicate_count, result.stdout.decode('utf-8'))
+    parse_output(data_size, predicate_count, result.stdout.decode('utf-8'))
+
+repetitions = 1
+for data_size in [40]:
+    for predicate_count in range(1, 513, 1):
+        run(data_size, predicate_count, repetitions)
